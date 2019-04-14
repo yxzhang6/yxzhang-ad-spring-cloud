ab322b1f304372068a404f6abac78d688d6a4217
yxzhang6 <yxzhang6@163.com>
2019-04-14 14:10:08 +0800

    SpringCloud项目初始化

---
+++ ad-eureka/src/main/java/com/yxzhang/ad/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.yxzhang.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by zhang on 2019/4/14.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
